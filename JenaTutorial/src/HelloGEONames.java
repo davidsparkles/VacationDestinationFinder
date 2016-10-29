2b397eee11f5a128ffd94b6ca1194cd51bb2c542
David Hentschel <David Hentschel>
2016-10-29 13:32:15 +0200

    first commit

---
+++ JenaTutorial/src/HelloGEONames.java
@@ -0,0 +1,52 @@
+import java.util.Iterator;
+
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+public class HelloGEONames {
+
+	public static void main(String[] args) {
+		
+		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+				"PREFIX rdfs: <https://www.w3.org/2000/01/rdf-schema#>\n" +
+				"PREFIX onto: <http://www.ontotext.com/proton/protontop#>\n" +
+				"PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+				"PREFIX dbr: <http://dbpedia.org/resource/>\n" +
+				"select distinct ?Continent ?o\n" + 
+				"where {\n" + 
+					"<http://sws.geonames.org/2873891/> onto:locatedIn ?Continent .\n" +
+//					"<http://dbpedia.org/resource/Earth> rdf:type ?o\n" +
+					"?Continent rdf:type dbo:Continent\n ." +
+					"?Continent dbo:language ?o\n" +
+				"}\n" + 
+				"LIMIT 100";
+		
+		Query query = QueryFactory.create(queryString);
+		
+		String serviceStringDBpedia = "http://dbpedia.org/sparql";
+		String serviceString = "http://factforge.net/sparql";
+		
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceString, query);
+		
+		try{
+			ResultSet results = qexec.execSelect();
+			while(results.hasNext()){
+				QuerySolution result = results.next();
+//				if(result.contains("settlement")) 
+				System.out.println(result.get("Continent") + " - " + result.get("p") + " - " + result.get("o"));
+			}
+		} finally {
+			qexec.close();
+		}
+	}
+
+}
---
+++ JenaTutorial/src/HelloRDFWorld.java
@@ -0,0 +1,63 @@
+import java.util.Iterator;
+
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+public class HelloRDFWorld {
+
+	public static void main(String[] args) {
+		
+//		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+//				"PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+//				"PREFIX dbr: <http://dbpedia.org/resource/>\n" +
+//				"select distinct ?settlement ?populationTotal\n" + 
+//				"where {\n" + 
+//					"?settlement rdf:type dbo:City ;\n" + 
+//						"dbo:populationTotal ?populationTotal ;\n" +
+//						"dbo:country dbr:Germany .\n" +
+//					"FILTER ( ?populationTotal > 1000000 )\n" +
+//				"}\n" + 
+//				"LIMIT 100";
+		
+		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+				"PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+				"PREFIX dbp: <http://dbpedia.org/property/>\n" +
+				"PREFIX dbr: <http://dbpedia.org/resource/>\n" +
+//				"select distinct ?Concept where {dbr:Mannheim <http://dbpedia.org/property/janLowC> ?Concept} LIMIT 100"
+				"select distinct ?p\n" + 
+				"where {\n" + 
+					"dbr:Mannheim dbp:junHighC ?p\n" + 
+//						"dbo:populationTotal ?populationTotal ;\n" +
+//						"dbo:country dbr:Germany .\n" +
+//					"FILTER ( ?populationTotal > 1000000 )\n" +
+				"}\n" + 
+				"LIMIT 100"
+		;
+		
+		Query query = QueryFactory.create(queryString);
+		
+		String serviceStringDBpedia = "http://live.dbpedia.org/sparql";
+		
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceStringDBpedia, query);
+		
+		try{
+			ResultSet results = qexec.execSelect();
+			while(results.hasNext()){
+				QuerySolution result = results.next();
+				if(result.contains("p")) System.out.println(result.get("p") + " - " + result.get("populationTotal"));
+			}
+		} finally {
+			qexec.close();
+		}
+	}
+
+}
