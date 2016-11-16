4b9e02fb338e794967e3bdc46199b9666c94fa25
Felix Kath <Felix.Kath@sap.com>
2016-11-16 14:42:33 +0100

    added algorithm to get distance based on lat long coordinates

--- vacationDestination/src/main/java/org/semantic/vacationDestination/api/generated/DefaultDestinationResource.java
+++ vacationDestination/src/main/java/org/semantic/vacationDestination/api/generated/DefaultDestinationResource.java
@@ -37,6 +37,13 @@ public class DefaultDestinationResource implements org.semantic.vacationDestinat
 	{
 		String returnString="";
 		// place some logic here
+		
+		
+		//if airplane - travelspeed is 762km/h
+		//if car - travelspeed is 89,9km/h
+		
+		
+		
 		String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
 				"PREFIX rdfs: <https://www.w3.org/2000/01/rdf-schema#>\n" +
 				"PREFIX onto: <http://www.ontotext.com/proton/protontop#>\n" +
@@ -74,3 +81,26 @@ public class DefaultDestinationResource implements org.semantic.vacationDestinat
 	}
 
 }
+
+class HaversineAlgorithm {
+
+    static final double _eQuatorialEarthRadius = 6378.1370D;
+    static final double _d2r = (Math.PI / 180D);
+
+    public static int HaversineInM(double lat1, double long1, double lat2, double long2) {
+        return (int) (1000D * HaversineInKM(lat1, long1, lat2, long2));
+    }
+
+    public static double HaversineInKM(double lat1, double long1, double lat2, double long2) {
+        double dlong = (long2 - long1) * _d2r;
+        double dlat = (lat2 - lat1) * _d2r;
+        double a = Math.pow(Math.sin(dlat / 2D), 2D) + Math.cos(lat1 * _d2r) * Math.cos(lat2 * _d2r)
+                * Math.pow(Math.sin(dlong / 2D), 2D);
+        double c = 2D * Math.atan2(Math.sqrt(a), Math.sqrt(1D - a));
+        double d = _eQuatorialEarthRadius * c;
+
+        return d;
+    }
+
+}
+
